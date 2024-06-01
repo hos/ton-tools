dc1f2e4347d08c731798318cd1a659cfedd3b67b
hos <hovosanoyan@gmail.com>
2024-06-01 13:58:49 +0400

    chore: Refactor Watch class to use generic type for store

--- packages/ton-watch/src/index.ts
+++ packages/ton-watch/src/index.ts
@@ -3,16 +3,16 @@ import { pgClient, type Transactions } from "./stores/pg/pg-client";
 import { getLiteClient } from "./lite-client";
 import { Watch } from "./watch";
 import { PgStore } from "./stores";
-import { Cell, loadTransaction, type Transaction } from "@ton/core";
+import { Cell, loadTransaction } from "@ton/core";
 
 const address = "UQCJTkhd1W2wztkVNp_dsKBpv2SIoUWoIyzI7mQrbSrj_Ilk";
 
-const liteClient = await getLiteClient('mainnet');
+const liteClient = await getLiteClient("mainnet");
 
 const store = new PgStore(pgClient);
 const watch = new Watch({ liteClient, store });
 
-await watch.migrate({ drop: true });
+await watch.store.start({ drop: true });
 
 if (address) {
   await watch.store.setAddress(address, 0n);
@@ -26,17 +26,18 @@ await watch.start();
 
 const {
   rows: [{ boc }],
-} = await pgClient.query<Pick<Transactions, 'boc'>>(`select boc from transactions order by created_at desc limit 1`);
+} = await pgClient.query<Pick<Transactions, "boc">>(
+  `select boc from transactions order by created_at desc limit 1`
+);
 
-if(!boc)  {
-  throw new Error('No transactions found');
+if (!boc) {
+  throw new Error("No transactions found");
 }
 
 const cell = Cell.fromBoc(boc)[0];
 const tx = loadTransaction(cell.beginParse());
 
-console.log(`Last transaction: ${tx.hash().toString('hex')}`)
-
+console.log(`Last transaction: ${tx.hash().toString("hex")}`);
 
 // Cleanup on exit
 // await watch.close();
